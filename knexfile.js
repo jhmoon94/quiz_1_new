a0b158d4b13391f62bd1bc9e4948bb1ef94641c7
jhmoon94 <jhmoon94@yahoo.com>
2021-09-01 13:06:07 -0700

    problem

--- knexfile.js
+++ knexfile.js
@@ -3,42 +3,19 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
+      filename: 'cluckr'
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    tableName: 'migrations',
+    directory: './db/migrations'
   },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  seeds: {
+    directory: "./db/seeds"
   }
 
 };
--- server.js
+++ server.js
@@ -2,12 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const methodOverride = require('method-override');
 
 app.use(morgan('dev')); 
 app.use(express.urlencoded({ extended: true })); 
 app.use('/',express.static('./public')); 
-app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 
@@ -15,3 +13,6 @@ app.listen(process.env.PORT, () => {
     console.log(`Listening at ${process.env.PORT}...`);
 });
 
+app.get("/", (req,res) => {
+    res.render("index")
+})
